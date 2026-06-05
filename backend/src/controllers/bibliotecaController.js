21230f767243700459afb27df27c33d1fe77bf77
David <davillar.dv.321@gmail.com>
2026-06-05 10:16:44 -0300

    Agregue las funcionalidades de crear y mostrar todo en los controladores de biblioteca y tambien cree el archivo en donde van las rutas del sistema

---
+++ backend/src/controllers/bibliotecaController.js
@@ -0,0 +1,15 @@
+import * as bibliotecaServices from "../services/libroService.js";
+
+export const anhadirLibro = async (req,res) => {
+    try {
+        const {titulo,autor,genero} = req.body;
+        const libro = await bibliotecaServices.registrarLibro(titulo,autor,genero);
+        res.status(201).json({mensaje:"Libro agregado correctamente",libro});
+    } catch (error) {
+        res.status(400).json({mensaje:error.message});
+    }
+};
+
+export const traerLibros = async (req,res) => {
+    return res.status(200).json(bibliotecaServices.obtenerLibros());
+};
